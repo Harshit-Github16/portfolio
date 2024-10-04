2883c4f3da593ddb7ba8d8c735151208f2b349d9
Harshit <harshit.geekologix@gmail.com>
2024-10-04 17:45:05 +0530

    change

--- src/Components/WelcomeSection.jsx
+++ src/Components/WelcomeSection.jsx
@@ -146,9 +146,9 @@ const WelcomeSection = () => {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ delay: 1.2, duration: 0.5 }}
-        className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center"
+        className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center max-md:hidden"
       >
-        <span className="text-gray-400 mb-2">Scroll to explore</span>
+        <span className="text-gray-400 mb-2 ">Scroll to explore</span>
         <FaMousePointer className="text-2xl animate-bounce" />
       </motion.div>
     </motion.section>
